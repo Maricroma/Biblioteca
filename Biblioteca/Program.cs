d9476cc93902bb74f5267016bde01c610fe0afba
Maricroma <cris.apdg@gmail.com>
2025-05-03 18:24:04 -0300

    Adding menu at Program.cs

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -7,43 +7,73 @@ namespace Biblioteca
         static void Main(string[] args)
         {
             Biblioteca biblioteca = new Biblioteca();
-            //cargarLibros(10);
-            biblioteca.AgregarLibro("El Principito", "Antoine de Saint-Exupéry", "Reynal & Hitchcock");
-            biblioteca.AgregarLibro("Cien años de soledad", "Gabriel García Márquez", "Editorial Sudamericana");
-            biblioteca.AgregarLibro("1984", "George Orwell", "Secker & Warburg");
-            biblioteca.AgregarLibro("El túnel", "Ernesto Sabato", "Editorial Sudamericana");
-            biblioteca.AgregarLibro("Rayuela", "Julio Cortázar", "Editorial Sudamericana");
-            biblioteca.AgregarLibro("El Aleph", "Jorge Luis Borges", "Editorial Losada");
-            biblioteca.ListarLibros();
-            //biblioteca.eliminarLibro("El Principito");
-            //biblioteca.listarLibros();
 
-            biblioteca.AltaLector("Juan Pérez", "12345678");
-            Console.WriteLine(biblioteca.PrestarLibro("1234567", "El Principito"));
-            Console.WriteLine(biblioteca.PrestarLibro("12345678", "Odisea"));
-            Console.WriteLine(biblioteca.PrestarLibro("12345678", "El Principito"));
-            biblioteca.ListarLibros();
+            Console.WriteLine("Bienvenido a la biblioteca virtual");
 
-            //void cargarLibros(int cantidad)
-            //{
-            //    for (int i = 0; i < cantidad; i++)
-            //    {
-            //        Console.WriteLine("Ingrese el título del libro:");
-            //        string titulo = Console.ReadLine();
-            //        Console.WriteLine("Ingrese el autor del libro:");
-            //        string autor = Console.ReadLine();
-            //        Console.WriteLine("Ingrese la editorial del libro:");
-            //        string editorial = Console.ReadLine();
-            //        if (biblioteca.agregarLibro(titulo, autor, editorial))
-            //        {
-            //            Console.WriteLine("Libro agregado exitosamente.");
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("El libro ya existe en la biblioteca.");
-            //        }
-            //    }
-            //}
+            while (true)
+            {
+                Console.WriteLine("\nMenú:");
+                Console.WriteLine("1. Agregar libro");
+                Console.WriteLine("2. Listar libros");
+                Console.WriteLine("3. Eliminar libro");
+                Console.WriteLine("4. Salir");
+                Console.Write("Seleccione una opción: ");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        AgregarLibro(biblioteca);
+                        break;
+                    case "2":
+                        biblioteca.ListarLibros();
+                        break;
+                    case "3":
+                        EliminarLibro(biblioteca);
+                        break;
+                    case "4":
+                        Console.WriteLine("Gracias por usar la biblioteca virtual. ¡Hasta luego!");
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+        }
+
+        static void AgregarLibro(Biblioteca biblioteca)
+        {
+            Console.WriteLine("Ingrese el título del libro:");
+            string titulo = Console.ReadLine();
+            Console.WriteLine("Ingrese el autor del libro:");
+            string autor = Console.ReadLine();
+            Console.WriteLine("Ingrese la editorial del libro:");
+            string editorial = Console.ReadLine();
+
+            if (biblioteca.AgregarLibro(titulo, autor, editorial))
+            {
+                Console.WriteLine("Libro agregado exitosamente.");
+            }
+            else
+            {
+                Console.WriteLine("El libro ya existe en la biblioteca.");
+            }
+        }
+
+        static void EliminarLibro(Biblioteca biblioteca)
+        {
+            Console.WriteLine("Ingrese el título del libro a eliminar:");
+            string tituloEliminar = Console.ReadLine();
+
+            if (biblioteca.EliminarLibro(tituloEliminar))
+            {
+                Console.WriteLine("Libro eliminado.");
+            }
+            else
+            {
+                Console.WriteLine("El libro no existe en la biblioteca.");
+            }
         }
     }
 }
